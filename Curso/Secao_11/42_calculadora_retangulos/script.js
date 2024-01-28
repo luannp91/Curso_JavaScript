1cfb16f7ce6cfe7f2708779c59543574ec8d28f1
Luan Pinto <luan.nelmo@hotmail.com>
2024-01-28 17:42:20 -0300

    Calculadora de Retangulos

---
+++ Curso/Secao_11/42_calculadora_retangulos/script.js
@@ -0,0 +1,16 @@
+class Retangulo {
+
+    constructor(largura, altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    calcularArea() {
+        return this.largura * this.altura;
+    }
+
+    calcularPerimetro() {
+        return 2 * (this.largura + this.altura);
+    }
+}
+
